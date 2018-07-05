eda1a5694cb6f440ab8faf7cd46dd79d60e5d2fe
Hiren Thacker <hithacker@gmail.com>
2018-07-05 14:33:27 +0530

    Fixing the only test that's there

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import {withRouter} from "react-router-dom";
 import './App.css';
 
 const App = (props) => {
-    const {header: Header, history, location, breadcrumb: Breadcrumb, content: Content} = props;
+    const {header: Header, history, location, match, breadcrumb: Breadcrumb, content: Content} = props;
     return <div>
         {Header && <Header/>}
         {Breadcrumb && <Breadcrumb location={location}/>}
         <div className="container">
-            <Content history={history}/>
+            <Content history={history} match={match}/>
         </div>
     </div>;
 };
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {MemoryRouter} from 'react-router-dom';
 import App from './App';
+import Dashboard from './components/Dashboard';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const app = <MemoryRouter><Dashboard /></MemoryRouter>
+  ReactDOM.render(app, div);
   ReactDOM.unmountComponentAtNode(div);
 });
