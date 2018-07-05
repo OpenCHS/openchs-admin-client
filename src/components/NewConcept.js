769c538c38c521995e677d1ccfea1c11e0f4d37f
Hiren Thacker <hithacker@gmail.com>
2018-07-05 13:03:08 +0530

    Removed useless divs that came from copy pasting :-)

--- src/components/NewConcept.js
+++ src/components/NewConcept.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Label, Form, FormGroup, Container, Col, Row, Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
 import Autosuggest from 'react-autosuggest';
-
-import SearchAnswersModal from './SearchAnswersModal';
+import uuidv4 from 'uuid/v4';
 
 function handleErrors(response) {
   if (!response.ok) {
@@ -48,7 +47,7 @@ class NewConcept extends React.Component {
   };
 
   onAddSuggestion = (event) => {
-    if(this.state.selectedSuggestion != null) {
+    if (this.state.selectedSuggestion != null) {
       this.setState(prevState => ({
         selectedAnswers: [...prevState.selectedAnswers, prevState.selectedSuggestion],
         autoSuggestValue: '',
@@ -104,8 +103,15 @@ class NewConcept extends React.Component {
   };
 
   handleSubmit(event) {
+    let concept = { "uuid": uuidv4() };
+    concept["name"] = this.state.name;
+    concept["dataType"] = this.state.dataType;
+    concept["answers"] = this.state.selectedAnswers.map(sA => ({uuid:sA.uuid, name: sA.name}));
+    const concepts = [concept];
+
     //alert(JSON.stringify(this.state.selectedAnswers.map((sA) => sA.name)));
-    alert(JSON.stringify(this.state));
+    alert(JSON.stringify(concepts));
+    
     event.preventDefault();
   }
 
@@ -124,7 +130,7 @@ class NewConcept extends React.Component {
     };
     const optionItems = selectedAnswers.map((sA) =>
       <option key={sA.uuid} value={sA.uuid}>{sA.name}</option>
-    );    
+    );
 
     return (
       <div>
@@ -132,33 +138,33 @@ class NewConcept extends React.Component {
           <Row>
             <Col sm={8}>
               <form onSubmit={this.handleSubmit}>
-                <div className="modal-body">
-                  <div className="form-group has-danger">
-                    <label htmlFor="name">Name</label>
-                    <input type="text"
-                      className="form-control form-control-danger"
-                      id="name"
-                      value={this.state.name}
-                      name="name"
-                      placeholder="Enter concept name"
-                      onChange={this.onChangeField}
-                      required />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="dataType">Concept Type</label>
-                    <select
-                      className="form-control"
-                      id="dataType"
-                      value={this.state.dataType}
-                      name="dataType"
-                      onChange={this.onChangeField}>
-                      <option>NA</option>
-                      <option>Numeric</option>
-                      <option>Text</option>
-                      <option>Coded</option>
-                      <option>Date</option>
-                    </select>
-                  </div>
+                <div className="form-group has-danger">
+                  <label htmlFor="name">Name</label>
+                  <input type="text"
+                    className="form-control form-control-danger"
+                    id="name"
+                    value={this.state.name}
+                    name="name"
+                    placeholder="Enter concept name"
+                    onChange={this.onChangeField}
+                    required />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="dataType">Concept Type</label>
+                  <select
+                    className="form-control"
+                    id="dataType"
+                    value={this.state.dataType}
+                    name="dataType"
+                    onChange={this.onChangeField}>
+                    <option>NA</option>
+                    <option>Numeric</option>
+                    <option>Text</option>
+                    <option>Coded</option>
+                    <option>Date</option>
+                  </select>
+                </div>
+                {this.state.dataType === 'Coded' &&
                   <div className="form-group">
                     <label htmlFor="selectedAnswers">Selected Answers</label>
                     <div className="form-row">
@@ -177,39 +183,75 @@ class NewConcept extends React.Component {
                       </div>
                     </div>
                   </div>
-                  
-                </div>
-                <div className="modal-footer">
-                  <input
-                    className="btn btn-primary btn-block"
-                    type="submit"
-                    value="Create">
-                  </input>
-                </div>
+                }
+                {this.state.dataType === 'Numeric' &&
+                  <div className="form-row">
+                    <div className="form-group col-md-2">
+                      <label htmlFor="lowAbsolute">Low Absolute</label>
+                      <input type="text" className="form-control" id="lowAbsolute"
+                         />
+                    </div>
+                    <div className="form-group col-md-2">
+                      <label htmlFor="highAbsolute">High Absolute</label>
+                      <input type="text" className="form-control" id="highAbsolute"
+                         />
+                    </div>
+                    <div className="form-group col-md-2">
+                      <label htmlFor="lowNormal">Low Normal</label>
+                      <input type="text" className="form-control" id="lowNormal"
+                         />
+                    </div>
+                    <div className="form-group col-md-2">
+                      <label htmlFor="highNormal">High Normal</label>
+                      <input type="text" className="form-control" id="highNormal"
+                         />
+                    </div>
+                    <div className="form-group col-md-4">
+                      <label htmlFor="unit">Unit</label>
+                      <select id="unit" className="form-control"
+                        >
+                        <option defaultValue="">Choose...</option>
+                        <option>cm</option>
+                        <option>kg</option>
+                        <option>mm Hg</option>
+                        <option>g/dL</option>
+                        <option>beats/minute</option>
+                        <option>breaths/minute</option>
+                        <option>&#8451;</option>
+                        <option>&#8457;</option>
+                      </select>
+                    </div>
+                  </div>
+                }
+                <input
+                  className="btn btn-primary btn-block"
+                  type="submit"
+                  value="Create">
+                </input>
               </form>
-              <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-                    <ModalHeader toggle={this.toggle}>Choose an answer</ModalHeader>
-                    <ModalBody>
-                      <form onSubmit={(e) => e.preventDefault()}>
-                        <FormGroup>
-                          <Label for="findConcept">Find Concept</Label>
-                          <Autosuggest
-                            id="findConcept"
-                            suggestions={suggestions}
-                            onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-                            onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-                            onSuggestionSelected={this.onSuggestionSelected}
-                            getSuggestionValue={getSuggestionValue}
-                            renderSuggestion={renderSuggestion}
-                            inputProps={inputProps} />
-                        </FormGroup>
-                      </form>
-                    </ModalBody>
-                    <ModalFooter>
-                      <Button color="primary" onClick={this.onAddSuggestion}>Add</Button>{' '}
-                      <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-                    </ModalFooter>
-                  </Modal>
+              <Modal isOpen={this.state.modal} toggle={this.toggle}>
+                <ModalHeader toggle={this.toggle}>Choose an answer</ModalHeader>
+                <ModalBody>
+                  <form onSubmit={(e) => e.preventDefault()}>
+                    <FormGroup>
+                      <Label for="findConcept">Find Concept</Label>
+                      <Autosuggest
+                        id="findConcept"
+                        suggestions={suggestions}
+                        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                        onSuggestionSelected={this.onSuggestionSelected}
+                        getSuggestionValue={getSuggestionValue}
+                        renderSuggestion={renderSuggestion}
+                        inputProps={inputProps} />
+                    </FormGroup>
+                  </form>
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="primary" onClick={this.onAddSuggestion}>Add</Button>{' '}
+                  <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                </ModalFooter>
+              </Modal>
             </Col>
           </Row>
         </Container>
